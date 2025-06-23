1e1e1dcc0803c3fd1ff626a825b9d0907a760bc8
Satrajit Ghosh <satra@mit.edu>
2025-06-23 11:46:12 +1000

    add deploy prefix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ReproInventory/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
